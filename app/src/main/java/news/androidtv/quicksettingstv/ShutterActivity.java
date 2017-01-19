91e3fccfb6355c2541c6d9fb1c1b74e7ede82fd2
Nick <handnf@gmail.com>
2017-01-19 01:01:18 -0500

    Realized that this won't work

--- app/src/main/java/news/androidtv/quicksettingstv/ShutterActivity.java
+++ app/src/main/java/news/androidtv/quicksettingstv/ShutterActivity.java
@@ -9,15 +9,18 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
 
 /**
@@ -49,10 +52,13 @@ public class ShutterActivity extends Activity {
         ((Switch) findViewById(R.id.option_start)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!mStart && isChecked && mHandler != null) {
+                    mHandler.sendEmptyMessageDelayed(0, mDelay);
+                }
                 mStart = isChecked;
                 if (isChecked && mHandler == null) {
                     mHandler = new ScreenshotHandler();
-                    mHandler.sendEmptyMessage(0);
+                    mHandler.sendEmptyMessageDelayed(0, mDelay);
                 }
             }
         });
@@ -96,8 +102,8 @@ public class ShutterActivity extends Activity {
                 return;
             }
             saveScreenshot();
-            if (mStart) {
-                sendEmptyMessageDelayed(0, mDelay);
+            if (mBurst && mStart) {
+                sendEmptyMessageDelayed(0, mDelay * 1000);
             }
         }
 
@@ -112,11 +118,13 @@ public class ShutterActivity extends Activity {
 
         private void saveScreenshot() {
             Date now = new Date();
-            android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+            CharSequence filename = DateFormat.format("yyyy-MM-dd__hh_mm_ss", now);
 
             try {
                 // image naming and path  to include sd card  appending name you choose for file
-                String mPath = Environment.getExternalStorageDirectory().toString() + "/PICTURES/Screenshots/" + now + ".jpg";
+                String mPath = Environment.getExternalStorageDirectory().toString() + "/PICTURES/Screenshots/" + filename + ".jpg";
+                mPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                        + "/screenshot_" + filename.toString() + ".jpg";
 
                 File imageFile = new File(mPath);
 
@@ -126,6 +134,8 @@ public class ShutterActivity extends Activity {
                 outputStream.flush();
                 outputStream.close();
                 Log.d(TAG, "Saving file " + mPath);
+                Toast.makeText(ShutterActivity.this, "Saved screenshot " + mPath,
+                        Toast.LENGTH_SHORT).show();
             } catch (Throwable e) {
                 // Several error may come out with file handling or OOM
                 e.printStackTrace();
